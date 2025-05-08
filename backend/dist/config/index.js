b2128dd5f468c63122f0a520cc69167f57345fa6
thaha <thahayaseen>
2025-05-08 23:11:58 +0530

    remove the dist folder

--- backend/dist/config/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.env = void 0;
-var env_config_1 = require("../config/env.config");
-Object.defineProperty(exports, "env", { enumerable: true, get: function () { return env_config_1.env; } });
-//# sourceMappingURL=index.js.map
